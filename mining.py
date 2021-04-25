b4ec1e6944e4484cb14f9a637fa0827c695de8d3
llalma <lhulsmanbenson@gmail.com>
2021-04-25 19:59:00 +1000

    merged testing and mining

--- mining.py
+++ mining.py
@@ -45,7 +45,7 @@ from functools import lru_cache
 from numbers import Number
 
 import search
-
+import random as rand
 import time
 
 
@@ -934,33 +934,419 @@ def formatResults(mine, state, actions):
     return state, actions
 #end
 
+##########################################
+#   Testing
+##########################################
+
+def surface_neigbhours2DTest():
+
+    input = np.array([[-1, -1, 10], [-1, 20, 4], [-1, -1, -1]])
+    mine = Mine(input)
+
+    loc = (1, 0)
+    actualNeighbours = Mine.surface_neigbhours(mine, loc)
+
+    expectedNeighbours = [(0, 0), (2, 0)]
+    assert expectedNeighbours == actualNeighbours
+# end
+
+def surface_neigbhours3DTest():
+
+    x = np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]])
+    input = np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], x - 1])
+    mine = Mine(input)
+
+    loc = (1, 1)
+    actualNeighbours = Mine.surface_neigbhours(mine, loc)
+    expectedNeighbours = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 2)]
+    assert expectedNeighbours == actualNeighbours
+# end
+
+def actions2DTest():
+    input = np.array([[-1, -1, 10], [-1, 20, 4], [-1, -1, -1]])
+    mine = Mine(input)
+
+    state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+    expectedActions = [(0, 0), (1, 0), (2, 0)]
+    actualActions = mine.actions(state)
+
+    for i, child in enumerate(actualActions):
+        print(child)
+        assert child == expectedActions[i]
+    # end
+# end
+
+def actions3DTest():
+    x = np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]])
+    input = np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], x - 1])
+    mine = Mine(input)
+
+    y = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    state = np.array([[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], y])
+    # #state = np.expand_dims(state, 1)
+    actualActions = mine.actions(state)
+    expectedActions = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+
+    for i, child in enumerate(actualActions):
+        print(child)
+        assert child == expectedActions[i]
+
+def emptyTupleTest():
+    input = np.array([[]])
+    mine = Mine(input)
+
+    state = np.array([[]])
+    # state = np.expand_dims(state, 1)
+    expectedActions = None
+    actualActions = mine.actions(state)
+
+    for i, child in enumerate(actualActions):
+        print(child)
+        assert child == expectedActions
+
+    # end
+# end
+
+def oneTupleValueTest():
+    input = np.array([[33]])
+    mine = Mine(input)
+    state = np.array([[0]])
+    # state = np.expand_dims(state, 1)
+    expectedActions = [(0, 0)]
+    actualActions = mine.actions(state)
+
+    for i, child in enumerate(actualActions):
+        print(child)
+        assert child == expectedActions[i]
+    # end
+# end
+
+def resultTest():
+    input = np.array([[-1, -1, 10], [-1, 20, 4], [-1, -1, -1]])
+    mine = Mine(input)
+
+    state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+    expectedActions = ((np.array([1, 1, 1]),), (np.array([0, 0, 0]),), (np.array([0, 0, 0]),))
+
+    actualActions = mine.result(state, (0, 0))
+
+    assert np.array(actualActions).all() == np.array(expectedActions).all()
+# end
+
+def resultsTest():
+    input = np.array([[-1, -1, 10], [-1, 20, 4], [-1, -1, -1]])
+    mine = Mine(input)
+
+    state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+    expectedActions = [[[2, 2, 2]], [[0, 0, 0]], [[0, 0, 0]]]
+
+    actualActions = mine.results(state, (0, 0))
+
+    assert np.array(actualActions).all() == np.array(expectedActions).all()
+# end
+
+def payoffTest():
+    input = np.array([[-2, -1, 10], [-1, 20, 7], [-1, -1, -1]])
+    mine = Mine(input)
+
+    state = np.array([[1, 1, 1], [1, 1, 1], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+
+    expectedValue = 33
+    actualValue = mine.payoff(state)
+
+    assert actualValue == expectedValue
+# end
+
+def is_dangerousTest():
+    input = np.array([[-2, -1, 10], [-1, 20, 7], [-1, -1, -1]])
+    mine = Mine(underground=input, dig_tolerance=1)
+
+    state = np.array([[1, 1, 1], [1, 1, 1], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+
+    expected = False
+    actual = mine.is_dangerous(state)
+    print(actual)
+
+    assert actual == expected
+# end
+
+def is_dangerousTest():
+    input = np.array([[-2, -1, 10], [-1, 20, 7], [-1, -1, -1]])
+    mine = Mine(underground=input, dig_tolerance=1)
+
+    state = np.array([[1, 1, 1], [1, 1, 1], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+
+    expected = True
+    actual = mine.is_dangerous(state)
+    print(actual)
+
+    assert actual == expected
+# end
+
+def back2DTest():
+    input = np.array([[-2, -1, 10], [-1, 20, 7], [-1, -1, -1]])
+    mine = Mine(input)
+    action = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+    state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+
+    # expected = False
+    [actualaction, actualstate] = mine.back2D(action, state)
+    print(actualaction)
+    print(actualstate)
+# end
+
+def validCoordsTest():
+    input = np.array([[-2, -1, 10], [-1, 20, 7], [-1, -1, -1]])
+    mine = Mine(input)
+
+    expected = False
+    actual = mine.validCoords((-1, -1, -1))
+    assert actual == expected
+
+    expected = True
+    actual = mine.validCoords((0, 0, 0))
+    assert actual == expected
+
+    expected = True
+    actual = mine.validCoords((2, 0, 2))
+    assert actual == expected
+
+    expected = False
+    actual = mine.validCoords((2, 1, 2))
+    assert actual == expected
+# end
+
+def validCoordsTest3D():
+
+    x = np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]])
+    input = np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], x - 1])
+    mine = Mine(input)
+
+    expected = False
+    actual = mine.validCoords((-1, -1, -1))
+    assert actual == expected
+
+    expected = True
+    actual = mine.validCoords((0, 0, 0))
+    assert actual == expected
+
+    expected = False
+    actual = mine.validCoords((2, 0, 2))
+    assert actual == expected
+
+    expected = False
+    actual = mine.validCoords((2, 1, 2))
+    assert actual == expected
+
+    expected = False
+    actual = mine.validCoords((2, 10, 2))
+    assert actual == expected
+# end
+
+def getRingCoords2DTest():
+
+    input = np.array([[-2, -1, 10], [-1, 20, 7], [-1, -1, -1]])
+    mine = Mine(input)
+
+    expected = [(2, 0, 0), (0, 0, 0)]
+    actual = getRingCoords(mine, (1, 0, 1))
+    assert actual == expected
+
+    expected = [(2, 0, 0), (1, 0, 0), (0, 0, 0)]
+    actual = getRingCoords(mine, (1, 1, 1))
+    assert actual == expected
+
+    expected = []
+    actual = getRingCoords(mine, (-1, 0, 0))
+    assert actual == expected
+# end
+
+def compareTest():
+    numTries = 2000  # rand.randint(5,10)
+    outputList = []
+
+    for _ in range(numTries):
+        print(_)
+        if rand.choice([True, False]):
+            # 2D mine test
+            size = tuple(np.random.randint(1, 5, (2)))
+        else:
+            # 3D mine test
+            size = tuple(np.random.randint(1, 3, (3)))
+        # end
+
+        digTolerance = rand.randint(1, 10)
+        underground = np.random.randint(-10, 10, size)
+
+        mine = Mine(underground, digTolerance)
+
+        dpResult = search_dp_dig_plan(mine)
+        bbResults = search_bb_dig_plan(mine)
+
+        outputList.append(str(dpResult) == str(bbResults))
+
+        if outputList[-1] != True:
+            print("")
+            dpResult = search_dp_dig_plan(mine)
+    # end
+
+    assert (False not in outputList)
+# end
+
+def getRingCoords3DTest():
+
+    x = np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]])
+    input = np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], x - 1])
+    mine = Mine(input)
+
+    expected = [(1, 1, 0), (0, 2, 0), (0, 1, 0)]
+    actual = getRingCoords(mine, (1, 2, 1))
+    assert actual == expected
+
+    expected = [(1, 2, 0), (1, 0, 0), (0, 2, 0), (0, 1, 0), (0, 0, 0)]
+    actual = getRingCoords(mine, (1, 1, 1))
+    assert actual == expected
+
+    expected = []
+    actual = getRingCoords(mine, (-1, 0, 0))
+    assert actual == expected
+# end
+
+def getParentsSumTest2D():
+
+    input = np.array([[-1, -1, -1], [-1, 4, -1], [-1, -10, 11]])
+    mine = Mine(input)
+
+    state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+    coords = (1, 0, 0)
+    prevSeenLocs = {}
+
+    expectedsum = -1
+    expectedPath = [(1, 0, 0)]
+    Sum, Path = getParentsSum(mine, state, coords, prevSeenLocs)
+
+    assert expectedsum == Sum
+    assert expectedPath == Path
+# end
+
+def getParentsSumTest2D():
+
+    input = np.array([[-1, -1, -1], [-1, 4, -1], [-1, -10, 11]])
+    mine = Mine(input)
+
+    state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    # state = np.expand_dims(state, 1)
+    coords = (1, 0, 0)
+    prevSeenLocs = {}
+
+    expectedsum = -1
+    expectedPath = [(1, 0, 0)]
+    Sum, Path = getParentsSum(mine, state, coords, prevSeenLocs)
+
+    assert expectedsum == Sum
+    assert expectedPath == Path
+# end
+
+def hardCodedBBMineTests():
+    mines = []
+
+    mines.append(np.array([[-1, -200, 1], [5, 8, 5]]))
+    mines.append(np.array([[-1, -1, -1], [-1, 4, -1], [-1, -10, 11]]))
+    mines.append(np.array([[-1, -1, 10, 5], [-1, -20, -4, -7], [-1, -1, -1, -21]]))
+    mines.append(np.array([[1, 4], [2, 5], [3, 6]]))
+    mines.append(np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]]))
+    mines.append(np.array([[1, -6, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1], [3, 6, 1, -10]]))
+    mines.append(np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], mines[-1-1] - 1]))
+
+    expectedResults = [12, 2, 0, 21, 26, 28, 42]
+
+
+    for i,m in enumerate(mines):
+        mine = Mine(m, dig_tolerance=1)
+
+        # print(search_bb_dig_plan(mine)[0])
+        bbResult = search_bb_dig_plan(mine)
+
+        assert bbResult[0] == expectedResults[i]
+    #end
+#end
+
+def hardCodedDPMineTests():
+    mines = []
+
+    mines.append(np.array([[-1, -200, 1], [5, 8, 5]]))
+    mines.append(np.array([[-1, -1, -1], [-1, 4, -1], [-1, -10, 11]]))
+    mines.append(np.array([[-1, -1, 10, 5], [-1, -20, -4, -7], [-1, -1, -1, -21]]))
+    mines.append(np.array([[1, 4], [2, 5], [3, 6]]))
+    mines.append(np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]]))
+    mines.append(np.array([[1, -6, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1], [3, 6, 1, -10]]))
+    mines.append(np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], mines[-1-1] - 1]))
+
+    expectedResults = [12, 2, 0, 21, 26, 28, 42]
+
+
+    for i,m in enumerate(mines):
+        mine = Mine(m, dig_tolerance=1)
+
+        # print(search_bb_dig_plan(mine)[0])
+        bbResult = search_bb_dig_plan(mine)
+
+        assert bbResult[0] == expectedResults[i]
+    #end
+#end
+
+def runTests():
+    # surface_neigbhours2DTest()
+    # surface_neigbhours3DTest()
+    # actions2DTest()
+    # actions3DTest()
+    # emptyTupleTest()
+    # oneTupleValueTest()
+    # resultTest()
+    # resultsTest()
+    # payoffTest()
+    # is_dangerousTest()
+    # compareTest()
+    # back2DTest()
+    # validCoordsTest()
+    # validCoordsTest3D()
+    # getRingCoords2DTest()
+    # getRingCoords3DTest()
+    # getParentsSumTest2D()
+
+    hardCodedBBMineTests()
+    hardCodedDPMineTests()
+#end
+
+##########################################
+#  End Testing
+##########################################
 
 def main():
-    # print(my_team())
+    runTests()
 
     tempArr = np.array([[-7 , 6 , 2 , 8], [-3, -7  ,5  ,1]])
-
-    b = np.array([[-1, -200, 1], [5, 8, 5]])
-    v = np.array([[-1, -1, -1], [-1, 4, -1], [-1, -10, 11]])
-    vDash = np.array([[-1, -1, 10, 5], [-1, -20, -4, -7], [-1, -1, -1, -21]])
-    w = np.array([[1, 4], [2, 5], [3, 6]])
-    x = np.array([[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, -1]])
-    y = np.array([[1, -6, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1], [3, 6, 1, -10]])
-    z = np.array([[[1, 4, 1, 1], [2, 5, 1, 1], [3, 6, 1, 1]], x - 1])
-
     mine = Mine(underground=tempArr, dig_tolerance=2)
 
     print(mine.underground)
 
     print("########################\ndpMethod\n########################")
-    best_action_list, best_payoff, best_final_state = search_dp_dig_plan(mine)
+    best_payoff, best_action_list , best_final_state = search_dp_dig_plan(mine)
     print(best_action_list)
     print(best_final_state)
     print(best_payoff)
 
     print("########################\nbbMethod\n########################")
 
-    best_action_list, best_payoff, best_final_state = search_bb_dig_plan(mine)
+    best_payoff, best_action_list, best_final_state = search_bb_dig_plan(mine)
     print(best_action_list)
     print(best_final_state)
     print(best_payoff)
